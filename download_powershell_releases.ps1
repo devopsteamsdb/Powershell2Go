5ce1bf0a7b9099ed9a3ebe0514acfaf21b3a172d
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2022-05-27 17:46:32 +0300

    Update and rename download_powershell_realses.ps1 to download_powershell_releases.ps1

---
+++ download_powershell_releases.ps1
@@ -0,0 +1,19 @@
+$AllPowerShellReleases = Invoke-RestMethod https://api.github.com/repos/PowerShell/PowerShell/releases
+
+foreach ($release in $AllPowerShellReleases)
+{
+  $PowershellVersion = $realse.tag_name.Substring(1) -as [version]
+  #$release.html_url
+  if($release.name.Contains('preview'))  { continue }
+  #$release.name
+  
+  $AllLinks = $release.assets.browser_download_url | where {$_.contains('win-x64.msi') -or $_.contains('rhel.7') }
+  
+  foreach ($link in $AllLinks)
+  {
+    wget $link
+  }
+  
+}
+
+
